6c060587b5caec80c0a5a05683659084c96efa34
KigKrazy <KigKrazy@163.com>
2017-12-18 17:50:48 +0800

    添加架构图

---
+++ util/src/main/java/com/reizx/andrutil/SysShell.java
@@ -0,0 +1,12 @@
+package com.reizx.andrutil;
+
+/**
+ * 一些系统命令
+ * Created by kig on 2017/12/18.
+ */
+
+public class SysShell {
+    public static void RequestPermission(String pkgName, String perm){
+
+    }
+}
