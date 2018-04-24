0bb8b8e065449451814f30059b301a9b13904179
KigKrazy <KigKrazy@163.com>
2018-04-24 09:48:02 +0800

    添加 httpurl解析接口

--- util/src/main/java/com/reizx/andrutil/HttpUrlUtil.java
+++ util/src/main/java/com/reizx/andrutil/HttpUrlUtil.java
@@ -2,7 +2,11 @@ package com.reizx.andrutil;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,19 +27,13 @@ public class HttpUrlUtil {
      * @param url
      * @return
      */
-    public static HashMap<String, String> getParamMaps(String url){
+    public static HashMap<String, String> getParamMaps(String url, boolean isUrlDecode){
         HashMap<String, String> paramMaps = new HashMap<>();
         String paramString = getParamString(url);
         if (StringUtils.isEmpty(paramString)){
             return paramMaps;
         }
-
-        String[] paramList = paramString.split("&");
-        for (String paramSub : paramList){
-            String[] paramSubKV = paramSub.split("=");
-            paramMaps.put(paramSubKV[0], paramSubKV[1]);
-        }
-        return paramMaps;
+        return form2Map(paramString, isUrlDecode);
     }
 
 
@@ -76,5 +74,69 @@ public class HttpUrlUtil {
             return matcher.group(2);
         }
         return null;
+    }
+
+    /**
+     * form表单数据转为map
+     * @param formString 表单数据
+     * @param isUrlDecode 是否对数据进行url解密
+     * @return
+     */
+    public static HashMap<String, String> form2Map(String formString, boolean isUrlDecode) {
+        try {
+            HashMap<String, String> paramMaps = new HashMap<>();
+
+            String[] paramList = formString.split("&");
+            for (String paramEl : paramList){
+                if (StringUtils.isEmpty(paramEl))
+                    continue;
+
+                String[] paramSubKV = paramEl.split("=");
+                String key = paramSubKV[0];
+                String val = paramSubKV[1];
+                if (isUrlDecode) {
+                    key = URLDecoder.decode(key, "utf-8");
+                    val = URLDecoder.decode(val, "utf-8");
+                }
+                paramMaps.put(key, val);
+            }
+            return paramMaps;
+        }catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+
+    }
+
+
+    /**
+     * map转为form数据
+     * @param paramMaps 数据map
+     * @param isUrlEncode 是否对数据进行url编码加密
+     * @return
+     */
+    public static String map2Form(HashMap<String, String> paramMaps, boolean isUrlEncode) {
+        try {
+            String rel = "";
+            Iterator<Map.Entry<String, String>> iter = paramMaps.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry<String, String> entry = iter.next();
+
+                String key = entry.getKey();
+                String val = entry.getValue();
+
+                if (isUrlEncode) {
+                    key = URLEncoder.encode(key, "utf-8");
+                    val = URLEncoder.encode(val, "utf-8");
+                }
+                rel += key + "=" + val + "&";
+            }
+            if (!rel.equals(""))
+                rel = rel.substring(0, rel.length() - 1);
+            return rel;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
     }
 }
